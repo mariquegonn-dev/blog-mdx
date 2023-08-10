2cfc780e95aab5f0d4e129b13f763a3599c23d2f
mariquegonn-dev <ocatodev@gmail.com>
2023-08-09 21:18:58 -0300

    build: jest & testing library added

---
+++ src/app/sum.spec.ts
@@ -0,0 +1,10 @@
+import { it } from "node:test";
+
+const sum = (a: number, b: number) => a + b;
+
+describe("sum", () => {
+  it("should returns 2", () => {
+    const result = sum(1, 1);
+    expect(result).toEqual(2);
+  });
+});
